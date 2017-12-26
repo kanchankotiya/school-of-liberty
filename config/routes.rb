1de464c5682cda4d50a7b93b312d73bccc072cd0
ravi4rails <ravionrails@gmail.com>
2017-12-26 19:01:22 +0530

    create our-vision page & general rules

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
    get '/about' => "home#about"
    get '/gallery' => "home#gallery"
    get '/contact' => "home#contact"
-   get '/admission-details' => "home#admission-details"
+   get '/general-rules' => "home#general-rules"
    get '/fees-structure' => "home#fees-structure"
+   get '/our-vision' => "home#our-vision"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
